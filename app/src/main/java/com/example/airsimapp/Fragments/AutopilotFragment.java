bc4e91404e25655813e8fbae2ba2ce4fa8334235
craft017 <craft017@csusm.edu>
2025-04-09 14:41:32 -0700

    Added logic for gathering user autopilot data

--- app/src/main/java/com/example/airsimapp/Fragments/AutopilotFragment.java
+++ app/src/main/java/com/example/airsimapp/Fragments/AutopilotFragment.java
@@ -13,12 +13,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.airsimapp.Activities.UserActivity;
 import com.example.airsimapp.GPS;
 import com.example.airsimapp.R;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 import java.util.Timer;
 
@@ -30,6 +33,9 @@ public class AutopilotFragment extends Fragment {
     public GPS tempGPS;
     private Handler speedUpdateHandler = new Handler(Looper.getMainLooper());
     private static final long UPDATE_INTERVAL = 1000;
+    public Date date = Calendar.getInstance().getTime();
+    public Calendar calendar = Calendar.getInstance();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -38,6 +44,25 @@ public class AutopilotFragment extends Fragment {
 
         // Get the button from the layout
         Button manualButton = view.findViewById(R.id.manualButton);
+        EditText latitude = view.findViewById(R.id.gpsCord);
+        EditText longitude = view.findViewById(R.id.gpsCord2);
+        EditText altitude = view.findViewById(R.id.gpsCord3);
+        EditText gpsTime = view.findViewById(R.id.gpsTime);
+        EditText headingSpeed = view.findViewById(R.id.headingSpeed);
+        EditText headingTime = view.findViewById(R.id.headingTime);
+        EditText patternTime = view.findViewById(R.id.patternTime);
+        Button addGPS = view.findViewById(R.id.addGPS);
+        Button addHeadingSpeed = view.findViewById(R.id.addHeadingSpeed);
+        Button addPattern = view.findViewById(R.id.addPattern);
+        addHeadingSpeed.setOnClickListener(v -> {
+            UserActivity.getOrchestrator().getAutopilot().addToCommandQueue(headingSpeed.toString(), headingTime.toString());
+        });
+        addGPS.setOnClickListener(v -> {
+            UserActivity.getOrchestrator().getAutopilot().addToCommandQueue(latitude.toString(), longitude.toString(), altitude.toString(), gpsTime.toString());
+        });
+        addPattern.setOnClickListener(v -> {
+            UserActivity.getOrchestrator().getAutopilot().addToCommandQueue();
+        });
 
 
         // Set the button's click listener to return to the UserPhoneFragment
