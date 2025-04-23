a3fbae5f2c0f88043320c55336b0294a96e92497
Jason <spencer.jason.us@gmail.com>
2025-04-23 15:14:36 -0700

    RaceTrack Pattern Implemented

--- app/src/main/java/com/example/airsimapp/LoiterPattern.java
+++ app/src/main/java/com/example/airsimapp/LoiterPattern.java
@@ -1,30 +1,46 @@
 package com.example.airsimapp;
-import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Objects;
 
 public class LoiterPattern extends AutopilotCommand{
-    private String pattern;
-    private ArrayList<String> commands = new ArrayList<String>();
     private int commandCounter;
+    private int timeForCommand = 200;   //Default 200 ms
+    private Pattern pattern;
+    private String patternType;
     public LoiterPattern(String Pattern, int hour, int minute){
         this.setId("LoiterPattern");
-        this.pattern = Pattern;
         this.commandCounter = 0;
         this.setHourEndTime(hour);
         this.setMinuteEndTime(minute);
+        this.patternType = Pattern;
     }
-private void loadCommands(){
-        if(this.pattern == "figureEight"){
 
-        }
-        else if(this.pattern == "raceTrack"){
-
-        }
+public void calculateCommand(float yawRate, float speed, Calendar startTime){
+    this.setPattern(patternType, yawRate, speed);
+    this.timeForCommand = this.pattern.currentCommandTime(this.commandCounter, yawRate, speed);
+    this.setCommandMessage(this.pattern.getCommands().get(commandCounter));
+    if(this.commandCounter >= this.pattern.getCommandLimit()){
+        this.commandCounter = 0;
+    }
+    else{
+        this.commandCounter++;
+    }
 }
-public void calculateCommand(float yawRate, float commandTime){
 
-}
-    public String getPattern() {
-        return pattern;
+    private void setPattern(String patternType, float yawRate, float speed) {
+        if(this.pattern == null){
+            if(Objects.equals(patternType, "RaceTrack")){
+                this.pattern = new RaceTrack(yawRate, speed);
+            }
+            else if(Objects.equals(patternType, "FigureEight")){
+                //TODO create figure eight pattern
+            }
+        }
+        else{
+            //Pattern already made, do nothing
+        }
+    }
+    public int getTimeForCommand() {
+        return timeForCommand;
     }
 }
