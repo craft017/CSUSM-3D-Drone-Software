4858555398d1843598a150f024cf76233cc21efd
Jason <spencer.jason.us@gmail.com>
2025-05-05 14:19:13 -0700

    Figure Eight Loiter pattern refactored

--- app/src/main/java/com/example/airsimapp/RaceTrack.java
+++ app/src/main/java/com/example/airsimapp/RaceTrack.java
@@ -2,30 +2,12 @@ package com.example.airsimapp;
 
 public class RaceTrack extends Pattern{
     public RaceTrack(float yawRate, float speed){
-        this.setCommandLimit(4);
-        this.loadCommands(yawRate, speed);
+        this.calculateStraightDistance(yawRate, speed);
+        this.degrees = 180;
+        this.setAllFlags(false);
     }
-    public void loadCommands(float yawRate, float speed){
-        float radius = this.radiusCalculation(yawRate, speed);
-        float rotationTime = this.rotationTime(yawRate);
-        float straightTime = this.straightTime(radius, speed);
-        this.commands.add("autopilot,forward_left," + yawRate + "," + speed + "," + rotationTime);
-        this.commands.add("autopilot,forward," + yawRate + "," + speed + "," + straightTime);
-        this.commands.add("autopilot,forward_left," + yawRate + "," + speed + "," + rotationTime);
-        this.commands.add("autopilot,forward," + yawRate + "," + speed + "," + straightTime);
-    }
-    public int currentCommandTime(int index, float yawRate, float speed){
-        if(index == 0 || index == 2){
-            return (int)this.rotationTime(yawRate);
-        }
-        else if(index == 1 || index == 3){
-            return (int)this.straightTime((this.radiusCalculation(yawRate, speed)), speed);
-        }
-        else{
-            return 0;
-        }
-    }
-    private float straightTime(float radius, float speed){
-        return (float) (2*radius)/speed;
+    private void calculateStraightDistance(float yawRate, float speed){
+        float r = this.calculateRadius(yawRate, speed);
+        this.straightDistance = 2*r;
     }
 }
